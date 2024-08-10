5be453cd038b35f41eacdf1a9e505fcce66421cb
VitaPhoneCZ <vitek.f18@gmail.com>
2024-08-11 00:54:04 +0200

    11.8.
    0:54

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.expandable').forEach(function(expandable) {
+    expandable.addEventListener('click', function() {
+        var content = this.nextElementSibling;
+        content.classList.toggle('show');
+    });
+});
