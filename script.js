6555b8c264a8fd1645898a5b4456e450d8534136
VitaPhoneCZ <vitek.f18@gmail.com>
2024-08-13 23:31:27 +0200

    animace sipky

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ document.querySelectorAll('.expandable').forEach(function(expandable) {
     expandable.addEventListener('click', function() {
         var content = this.nextElementSibling;
         content.classList.toggle('show');
+        this.classList.toggle('active');
     });
 });
