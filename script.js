149da995228cfbc7e8c961bb9a26a9550b80efaf
VitaPhone <vitek.f18@gmail.com>
2024-09-30 15:09:48 +0200

    dodelani obsahu

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 document.querySelectorAll('.expandable').forEach(function(expandable) {
-    expandable.addEventListener('click', function() {
-        var content = this.nextElementSibling;
-        content.classList.toggle('show');
-        this.classList.toggle('active');
-    });
+	expandable.addEventListener('click', function() {
+		var content = this.nextElementSibling;
+		content.classList.toggle('show');
+		this.classList.toggle('active');
+	});
 });
+
 function scrollToEuthanasia() {
-    document.getElementById('euthanasia').scrollIntoView({ behavior: 'smooth' });
+	document.getElementById('euthanasia').scrollIntoView({
+		behavior: 'smooth'
+	});
 }
+
 function scrollToPrevence() {
-    document.getElementById('prevence').scrollIntoView({ behavior: 'smooth' });
+	document.getElementById('prevence').scrollIntoView({
+		behavior: 'smooth'
+	});
+}
+
+function scrollToAparat() {
+	document.getElementById('aparat').scrollIntoView({
+		behavior: 'smooth'
+	});
 }
